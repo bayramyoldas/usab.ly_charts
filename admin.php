246829a0061527ae7d60130c11dac267b7242866
bayramyoldas <1306090007@ogr.iu.edu.tr>
2015-02-26 00:47:13 +0200

    menu

--- admin.php
+++ admin.php
@@ -12,16 +12,16 @@
 	<meta name="keywords" content="image, fan art, draw, game, anime, manga" />
 	<meta name="description" content="" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
 	<link href="css/style.css" rel="stylesheet">
+	<link href="css/font-awesome.css" rel="stylesheet">
 	<script href="js/prefixfree-1.0.7.js" type="text/javascript"></script>
-	<script href="js/jquery-1.7.1.min.js" type="text/javascript"></script>
+	<script src="js/jquery-1.7.1.min.js" type="text/javascript"></script>
 	<script>
 	/*jQuery*/
 	$(document).ready(function(){
 		$("#accordion_menu h3").click(function(){
 			//slide up all the link lists
-			$("#accordion_menu ul ul").slideUp();
+			$("#accordion_menu ul div").slideUp();
 			//slide down the link list below the h3 clicked - only if its closed
 			if(!$(this).next().is(":visible"))
 			{
@@ -32,37 +32,37 @@
 	</script>
 </head>
 <body>
-<div class="header"> </div>
+<div class="header"> HEADER</div>
 <div class="container">
 	<div id="accordion_menu">
 		<ul>
 			<li>
-				<h3><span class="icon-dashboard"></span>Pie Chart</h3>
-				Pie Chart Form
+				<h3><i class="fa fa-pie-chart"></i>Pie Chart</h3>
+				<div>Pie Chart Form</div>
 			</li>
 			<li>
-				<h3><span class="icon-dashboard"></span>Column Chart</h3>
-				Pie Chart Form
+				<h3><i class="fa fa-pie-chart"></i></span>Column Chart</h3>
+				<div>Column Chart Form</div>
 			</li>
 			<li>
-				<h3><span class="icon-dashboard"></span>Bar Chart</h3>
-				Pie Chart Form
+				<h3><i class="fa fa-bar-chart"></i></span>Bar Chart</h3>
+				<div>Bar Chart Form</div>
 			</li>
 			<li>
-				<h3><span class="icon-dashboard"></span>Line Chart</h3>
-				Pie Chart Form
+				<h3><i class="fa fa-line-chart"></i></span>Line Chart</h3>
+				<div>Line Chart Form</div>
 			</li>
 			<li>
-				<h3><span class="icon-dashboard"></span>Area Chart</h3>
-				Pie Chart Form
+				<h3><i class="fa fa-area-chart"></i></span>Area Chart</h3>
+				<div>Area Chart Form</div>
 			</li>
 			<li>
-				<h3><span class="icon-dashboard"></span>Bubble Chart</h3>
-				Pie Chart Form
+				<h3><i class="fa fa-pie-chart"></i></span>Bubble Chart</h3>
+				<div>Bubble Chart Form</div>
 			</li>
 			<li>
-				<h3><span class="icon-dashboard"></span>Scatter Chart</h3>
-				Pie Chart Form
+				<h3><i class="fa fa-pie-chart"></i></span>Scatter Chart</h3>
+				<div>Scatter Chart Form</div>
 			</li>
 		</ul>
 	</div>
